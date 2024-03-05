39e44858a625ed2961101834ec9a660995d6837f
Guigo <rsbecker@inf.ufsm.br>
2024-03-05 13:42:20 -0300

    declarar variáveis

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const
+const opcoesTag = ["p", "h1", "h2", "h3", "h4", "h5", "h6", "span"];
+
+const opcoesContainer = document.querySelector(".options");
+const outputContainer = document.querySelector(".output");
+const tagsSeletor = document.getElementById("tags");
+const paragrafosSlider = document.getElementById("paragrafos");
+const palavrasSlider = document.getElementById("palavras");
+const paragrafosValor = document.getElementById("paragrafosValor");
+const palavrasValor = document.getElementById("palavrasValor");
